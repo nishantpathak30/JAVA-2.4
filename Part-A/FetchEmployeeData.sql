864009d23e8c22b41b86938cc4281d9a312834c4
nishantpathak30 <pathaknishant6868@gmail.com>
2025-10-29 10:25:36 +0530

    Create FetchEmployeeData.sql

---
+++ Part-A/FetchEmployeeData.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Employee (
+    EmpID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Salary DOUBLE
+);
+INSERT INTO Employee VALUES (101, 'Nandini', 60000),
+                            (102, 'Rohit', 55000),
+                            (103, 'Asha', 50000);
